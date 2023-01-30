639e9c7f436cec942681563bcd4a91856064b79f
sacibaby <111527520+sacibaby@users.noreply.github.com>
2023-01-30 16:25:01 +0530

    Update config.js

--- config.js
+++ config.js
@@ -21,14 +21,14 @@ global.APIKeys = {
 }
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-global.session = process.env.SESSION_ID || 'PUT HERE',
-global.ownername = process.env.OWNER_NAME === undefined ? "DarkWinzo" : process.env.OWNER_NAME
-let fake = process.env.OWNER_NUMBER === undefined ? '94775200935' : process.env.OWNER_NUMBER
+global.session = process.env.SESSION_ID || 'Tk1mS2FhR1I=',
+global.ownername = process.env.OWNER_NAME === undefined ? "🍁вℓα¢к ѕувєя-ѕυкι🍁" : process.env.OWNER_NAME
+let fake = process.env.OWNER_NUMBER === undefined ? '94770378874' : process.env.OWNER_NUMBER
 global.owner = [`${fake}`]
 global.ownernumber = [`${fake}`]
 global.premium = [`${fake}`]
 global.anticall = process.env.ANTICALL === undefined ? 'false' : process.env.ANTICALL
-global.packname = process.env.PACK_NAME === undefined ? 'Queen-MD' : process.env.PACK_NAME
+global.packname = process.env.PACK_NAME === undefined ? 'qυєєη-м∂' : process.env.PACK_NAME
 global.author = process.env.STICKER_AUTHOR_NAME === undefined ? 'DarkWinzo' : process.env.STICKER_AUTHOR_NAME
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
